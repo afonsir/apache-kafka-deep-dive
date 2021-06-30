54f76edb2dd2ae82fed2ce3c9b86d9aa91562e80
AfonSir <afonsir17@gmail.com>
2021-06-30 00:38:14 -0300

    feat: add idempotence producer configurations

--- kafka-app/src/main/java/com/github/afonsir/Producer.java
+++ kafka-app/src/main/java/com/github/afonsir/Producer.java
@@ -14,9 +14,15 @@ public class Producer {
         // Producer configs
         Properties properties = new Properties();
 
-        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty( ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers );
+        properties.setProperty( ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName() );
+        properties.setProperty( ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName() );
+
+        // Idempotence
+        properties.setProperty( ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true" );
+        properties.setProperty( ProducerConfig.ACKS_CONFIG, "all" );
+        properties.setProperty( ProducerConfig.RETRIES_CONFIG, Integer.toString(Integer.MAX_VALUE) );
+        properties.setProperty( ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5" );
 
         // create producer
         KafkaProducer<String, String> producer = new KafkaProducer<String, String>( properties );
